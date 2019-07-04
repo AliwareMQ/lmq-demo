d3f8485a43360573b199ccdabf6897bb901ed64e
悟幻 <wuhuan.wp@alibaba-inc.com>
2019-07-04 21:41:23 +0800

    andriod demo

--- lmq-andriod-demo/app/src/main/java/mqtt/demo/Config.java
+++ lmq-andriod-demo/app/src/main/java/mqtt/demo/Config.java
@@ -2,7 +2,7 @@ package mqtt.demo;
 
 public class Config {
     public static final String serverUri = "xxx";
-    public static final String clientId = "xxx";
+    public static final String clientId = "GID_xxx@@@xxx";
     public static final String instanceId = "xxx";
     public static final String accessKey = "xxx";
     public static final String secretKey = "xxx";
